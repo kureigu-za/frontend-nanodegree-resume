6c608e520e698f12e4aab047b92e3573b0da80b3
Craig Oliver <craig.w.c.oliver@gmail.com>
2017-01-05 18:01:30 +0200

    Added all resume sections

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,84 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var work = {
+	"jobs" : [{
+			"employer" : "EDA",
+			"title" : "Software Developer",
+			"location" : "SU"
+			"date" : "2007"
+		}, {
+			"employer" : "ThirdRock",
+			"title" : "Software Developer",
+			"location" : "Wild West",
+			"date" : "2011"	
+		}
+	]
+};
+
+var projects = {
+	"projects" : [{
+			"title" : "BudgE",
+			"dates" : "2011",
+			"description" : "Mobile and web development of budgeting tool.",
+			"images" : ""
+		}, {
+			"title" : "Mining R&D",
+			"dates" : "2012",
+			"description" : "Support of mining related software, etc...",
+			"images" : ""
+		}
+	]
+};
+
+var bio = {
+	"name" : "Yondo Starlord",
+	"role" : "Software Developer",
+	"contacts" : {
+		"email" : "yondu@gmail.com",
+		"mobile" : "083555555",
+		"github" : "",
+		"location" : "Wild West"
+	},
+	
+	"pic" : "images/zoro.png",
+	"welcome" : "Greetings all",
+	"skills" : ["Awesomeness", "opm", "blah"]
+};
+
+var education = {
+	"schools" : [{
+			"name" : "SU",
+			"location" : "RSA",
+			"date" : "2011",
+			"url" : "",
+			"majors" : ["Computer Science", "Applied Maths"]
+		}
+	],
+	"onlineCourses" : [{
+			"title" : "Javascript Basics",
+			"school" : "Udacity",
+			"dates" : 2017,
+			"url" : "http://www.udacity.com/course/ud804"
+		}
+	]
+};
+
+// var headerName = HTMLheaderName.replace("%data%", bio.name);
+// var headerRole = HTMLheaderRole.replace("%data%", bio.role);
+
+// var email = HTMLemail.replace("%data%", bio.contacts.email);
+// var mobile = HTMLmobile.replace("%data%", bio.contacts["mobile"]);
+// var pic = HTMLbioPic.replace("%data%", bio.pic);
+// var welcome = HTMLwelcomeMsg.replace("%data%", bio.welcome);
+// var skills = HTMLskills.replace("%data%", bio.skills);
+
+
+
+
+// $("#header").prepend(headerRole);
+// $("#header").prepend(headerName);
+// $("#header").append(email);
+// $("#header").append(mobile);
+// $("#header").append(pic);
+// $("#header").append(welcome);
+// $("#header").append(skills);
+
+// $("#main").append("Test");
